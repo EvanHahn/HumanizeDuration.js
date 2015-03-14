cebf637877a43f3535271b1612a6cfbe2253cc6e
Evan Hahn <me@evanhahn.com>
2015-03-13 18:20:01 -0700

    Minor: add spaces around operators in Catalan definition

--- humanize-duration.js
+++ humanize-duration.js
@@ -17,9 +17,9 @@
     ca: {
       year: function(c) { return "any" + ((c !== 1) ? "s" : ""); },
       month: function(c) { return "mes" + ((c !== 1) ? "os" : ""); },
-      week: function(c) { return "setman" + ((c !== 1) ? "es" :"a"); },
-      day: function(c) { return "di" + ((c !== 1) ? "es" :"a"); },
-      hour: function(c) { return "hor" + ((c !== 1) ? "es" :"a"); },
+      week: function(c) { return "setman" + ((c !== 1) ? "es" : "a"); },
+      day: function(c) { return "di" + ((c !== 1) ? "es" : "a"); },
+      hour: function(c) { return "hor" + ((c !== 1) ? "es" : "a"); },
       minute: function(c) { return "minut" + ((c !== 1) ? "s" : ""); },
       second: function(c) { return "segon" + ((c !== 1) ? "s" : ""); },
       millisecond: function(c) { return "milisegon" + ((c !== 1) ? "s" : "" ); }
