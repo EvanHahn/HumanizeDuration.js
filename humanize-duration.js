1995f942e6a3925b038c66b64b52a7b529d03e1c
Evan Hahn <me@evanhahn.com>
2015-05-29 10:25:49 -0400

    Minor style cleanup (no "yoda conditions")

--- humanize-duration.js
+++ humanize-duration.js
@@ -333,7 +333,7 @@
       return 0;
     } else if (Math.floor(c) !== c) {
       return 1;
-    } else if (2 <= c % 10 && c % 10 <= 4 && !(10 < c % 100 && c % 100 < 20)) {
+    } else if (c % 10 >= 2 && c % 10 <= 4 && !(c % 100 > 10 && c % 100 < 20)) {
       return 2;
     } else {
       return 3;
