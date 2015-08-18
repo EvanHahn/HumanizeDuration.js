a75f6cd0a86304a244b3f491696a0d49d050cfc5
Evan Hahn <me@evanhahn.com>
2015-08-18 09:48:10 -1000

    Minor: shorten getSlavicForm

--- humanize-duration.js
+++ humanize-duration.js
@@ -385,9 +385,9 @@
   function getSlavicForm(c) {
     if (Math.floor(c) !== c) {
       return 2;
-    } else if (c === 0 || (c >= 5 && c <= 20) || (c % 10 >= 5 && c % 10 <= 9) || c % 10 === 0) {
+    } else if ((c >= 5 && c <= 20) || (c % 10 >= 5 && c % 10 <= 9) || c % 10 === 0) {
       return 0;
-    } else if (c === 1 || c % 10 === 1) {
+    } else if (c % 10 === 1) {
       return 1;
     } else if (c > 1) {
       return 2;
