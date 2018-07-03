a7abe9a59af5db83a49829a954cfc4e9ed9d81a0
kdomagoj <kdomagoj2@gmail.com>
2018-07-03 19:52:40 +0200

    minutes fix

--- humanize-duration.js
+++ humanize-duration.js
@@ -155,10 +155,10 @@
         return 'sati'
       },
       m: function (c) {
-        if (c === 1) return 'minuta'
-        if (c === 12 || c  === 13 || c === 14) return 'minuta'
-        if (c % 10 === 2 || c % 10 === 3 || c % 10 === 4) return 'minute'
-        return 'minuta'
+        if (c === 1 || c === 12 || c  === 13 || c === 14 || c === 15 || c === 16 || c === 17 || c === 18 || c === 19  ) return 'minuta';
+        if (c % 10 === 0) return "minuta";
+        if (c > 20 && (c % 10 === 2 || c % 10 === 3 || c % 10 === 4)) return 'minute';
+        return 'minuta';
       },
       s: function (c) {
         if (c === 1) return 'sekunda'
