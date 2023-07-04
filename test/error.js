bafc8e152f06ab19bd791a439c91303c3dd95846
Evan Hahn <me@evanhahn.com>
2023-07-04 16:43:34 +0000

    Minor: use latest ECMAScript version in tests

--- test/error.js
+++ test/error.js
@@ -3,11 +3,7 @@ const assert = require("node:assert/strict");
 
 describe("error handling", function () {
   it("throws an error when passed a bad language in the function", function () {
-    function humanizingWith(options) {
-      return () => {
-        humanizeDuration(10000, options);
-      };
-    }
+    const humanizingWith = (options) => () => humanizeDuration(10000, options);
 
     assert.throws(humanizingWith({ language: "EN" }), Error);
     assert.throws(humanizingWith({ language: "bad language" }), Error);
@@ -23,12 +19,7 @@ describe("error handling", function () {
     const humanizer = humanizeDuration.humanizer({
       language: "bad language",
     });
-
-    function humanizing(options) {
-      return () => {
-        humanizer(10000, options);
-      };
-    }
+    const humanizing = (options) => () => humanizer(10000, options);
 
     assert.throws(humanizing(), Error);
     assert.throws(humanizing({ language: "EN" }), Error);
@@ -40,11 +31,8 @@ describe("error handling", function () {
   });
 
   it("should throw if fallbacks configuration is invalid", function () {
-    function humanizingWith(options) {
-      return () => {
-        humanizeDuration(10000, options);
-      };
-    }
+    const humanizingWith = (options) => () => humanizeDuration(10000, options);
+
     assert.throws(humanizingWith({ language: "es", fallbacks: [] }), Error);
     assert.throws(
       humanizingWith({ language: "es", fallbacks: undefined }),
