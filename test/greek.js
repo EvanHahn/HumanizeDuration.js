8403611bd4994a65be6acff6e2018a4a5ccafc45
Evan Hahn <me@evanhahn.com>
2023-07-09 16:32:38 +0000

    Simplify Greek alias test

--- test/greek.js
+++ test/greek.js
@@ -3,37 +3,15 @@
 const humanizeDuration = require("..");
 const { test } = require("node:test");
 const assert = require("node:assert/strict");
-const fs = require("node:fs");
-const path = require("node:path");
-const parseCsv = require("csv-parse").parse;
 
 // See https://github.com/EvanHahn/HumanizeDuration.js/issues/143
 // for more here.
 
-test('aliases "gr" to "el"', (t, done) => {
-  const greekPath = path.resolve(__dirname, "definitions", "el.tsv");
-
-  fs.readFile(greekPath, { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      return done(err);
-    }
-
-    parseCsv(data, { delimiter: "\t" }, (err, rows) => {
-      if (err) {
-        return done(err);
-      }
-
-      rows.forEach((row) => {
-        const ms = parseFloat(row[0]);
-
-        const humanizedGr = humanizeDuration(ms, { language: "gr" });
-        const humanizedEl = humanizeDuration(ms, { language: "el" });
-        assert.strictEqual(humanizedGr, humanizedEl);
-      });
-
-      done();
-    });
-  });
+test('aliases "gr" to "el"', () => {
+  assert.strictEqual(
+    humanizeDuration(123456, { language: "gr" }),
+    humanizeDuration(123456, { language: "el" })
+  );
 });
 
 test('does not include "gr" in getSupportedLanguages', () => {
