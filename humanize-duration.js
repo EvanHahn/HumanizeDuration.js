cd0b2cb44125916c5f172df52f0adc1f6118326d
Evan Hahn <me@evanhahn.com>
2025-05-12 14:51:42 +0000

    Minor: replace a `ts-ignore` with a cast

--- humanize-duration.js
+++ humanize-duration.js
@@ -1682,8 +1682,12 @@
           if (character === ".") {
             formattedCount += decimal;
           } else {
-            // @ts-ignore because `character` should always be 0-9 at this point.
-            formattedCount += digitReplacements[character];
+            formattedCount +=
+              digitReplacements[
+                /** @type {"0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"} */ (
+                  character
+                )
+              ];
           }
         }
       } else {
